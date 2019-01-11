4a97b5bc56cca48ccf4c4ce8b6541c4d1ba1ce22
mldn <415577051@qq.com>
2019-01-11 21:48:55 +0800

    Edit Hello.java : Delete 3 Line

--- Hello.java
+++ Hello.java
@@ -5,10 +5,5 @@ public static void main(String[] args) {
 	System.out.println("asd");
 	System.out.println("asd2");
 	System.out.println("asd2");
-	System.out.println("asd");
-	System.out.println("asd2");
-	System.out.println("asd2");
-	System.out.println("asd");
-	System.out.println("asd2");
-	System.out.println("asd2");
+
 }
