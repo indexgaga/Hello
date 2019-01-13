30e00829d90ca05a529c287bcd00288e3bf075f0
mldn <415577051@qq.com>
2019-01-14 01:00:06 +0800

    modified Hello.java And add class Admin File

---
+++ Admin.java
@@ -0,0 +1,4 @@
+import java.util.*;
+public Class Admin(){
+	List list = new ArrayList();
+}
--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 public static void main(String[] args) {
 	System.out.println("asd");
 	System.out.println("asd2");
+		System.out.println("asd");
+	System.out.println("asd2");
 
 
 }
