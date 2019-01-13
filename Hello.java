ec785d78622107564212b09c9e69d32ff94aad32
mldn <415577051@qq.com>
2019-01-14 01:37:09 +0800

    modified Hello.java In dev

--- Hello.java
+++ Hello.java
@@ -3,6 +3,9 @@ public static void main(String[] args) {
 	System.out.println("asd2");
 		System.out.println("asd");
 	System.out.println("asd2");
+	System.out.println("asd2");
+		System.out.println("asd");
+	System.out.println("asd2");
 
 
 }
