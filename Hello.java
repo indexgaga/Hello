a92ed05dc3589a7f9aa9da87b418032fb767f26f
mldn <415577051@qq.com>
2019-01-15 14:59:34 +0800

    Update Hello.java In Dev

--- Hello.java
+++ Hello.java
@@ -6,6 +6,10 @@ public static void main(String[] args) {
 	System.out.println("asd2");
 		System.out.println("asd");
 	System.out.println("asd2");
-
+		System.out.println("asd");
+	System.out.println("asd2");
+	System.out.println("asd2");
+		System.out.println("asd");
+	System.out.println("asd2");
 
 }
